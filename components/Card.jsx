40e3dd550cfa3131405ae56049eb75c64541db87
Vachan MN <vachanmn123@gmail.com>
2024-10-25 22:57:32 +0530

    Some last moment hange

--- components/Card.jsx
+++ components/Card.jsx
@@ -9,7 +9,11 @@ export default function Card({ imageUrl, title, description, socials }) {
       transition={{ duration: 0.5 }}
       className="flex flex-col justify-between items-center bg-white/75 p-12 rounded-lg"
     >
-      <img src={imageUrl} className="h-48 w-48 rounded-full bg-black" />
+      <img
+        src={imageUrl}
+        alt={title}
+        className="h-48 w-48 rounded-full bg-black"
+      />
       <div className="flex flex-col gap-2">
         <h3 className="text-xl font-semibold text-black">{title}</h3>
         <p className="text-gray-500">{description}</p>
